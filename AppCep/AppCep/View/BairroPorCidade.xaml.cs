6ecf3dd43c91459c2a3eaf8e76bd79f9f0f42140
Usuario <Usuario@CPD02-M15>
2023-02-23 15:23:10 -0300

    Atualização p2

--- AppCep/AppCep/View/BairroPorCidade.xaml.cs
+++ AppCep/AppCep/View/BairroPorCidade.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using AppCep.Model;
+using AppCep.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Schema;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,13 +15,32 @@ namespace AppCep.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairroPorCidade : ContentPage
     {
+       ObservableCollection<Cidade> lista_cidades= new ObservableCollection<Cidade>();
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
         public BairroPorCidade()
         {
             InitializeComponent();
+
+           pck_cidade.ItemsSource= lista_cidades;
+           lista_bairros.ItemsSource= lista_bairros;
         }
 
-        private void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
         {
+            try
+            {
+                Picker disparador= sender as Picker;
+                string estado_selecionado = disparador.SelectedItem as string;
+                List<Cidade> arr_cidades = await DataService.GetCidadesByEstado(estado_selecionado);
+                lista_cidades.Clear();
+                arr_cidades.ForEach( i => lista_cidades.Add(i));
+
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("ops", ex.Message, "ok");
+
+            }
 
         }
 
