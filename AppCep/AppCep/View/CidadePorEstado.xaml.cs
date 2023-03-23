414ae756799fc9cc74b07a405aa8d98269636010
aspiregabriela <aspiregabriela@gmail.com>
2023-03-23 14:36:08 -0300

    modificações

---
+++ AppCep/AppCep/View/CidadePorEstado.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AppCep.Model;
+using AppCep.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCep.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CidadePorEstado : ContentPage
+    {
+        public CidadePorEstado()
+        {
+            InitializeComponent();
+        }
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = (Picker)sender;
+
+                string uf = disparador.SelectedItem as string;
+
+                List<Cidade> lista_cidade = await DataService.GetCidadeByEstado(uf);
+
+                lst_cidade.ItemsSource = lista_cidade;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+
+    }
+}
---
+++ AppCep/AppCep/View/EnderecoPorCep.xaml.cs
@@ -0,0 +1,41 @@
+﻿using AppCep.Model;
+using AppCep.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCep.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnderecoPorCep : ContentPage
+    {
+        public EnderecoPorCep()
+        {
+            InitializeComponent();
+        }
+
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = (Picker)sender;
+
+                string uf = disparador.SelectedItem as string;
+
+                List<Endereco> lista_endereco = await GetEnderecoByE;
+
+                lst_endereco.ItemsSource = lista_endereco;
+
+            }
+            catch (Exception ex)
+            {
+               await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+    }
+}
